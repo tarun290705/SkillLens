ec6e89d819c63aa2babc899a14593077031da47f
tarun290705 <tarun.prakash290705@gmail.com>
2025-11-03 16:04:37 +0530

    msg

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://127.0.0.1:8000/api";
+
+export const registerUser = async (userData) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/register/`, userData);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { error: "Something went wrong" };
+  }
+};
